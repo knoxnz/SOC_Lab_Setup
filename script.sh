af8e595fe48da356c561d5f5a8283ba49afba26b
Al Musabbir Abir <amabir2822@gmail.com>
2026-06-19 20:46:19 +0600

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,28 @@
+# System Requirements
+Before running the script, please ensure that your system meets the following requirements:
+
+* Ubuntu OS is perfect but you can Do this In Kali Linux
+* Minimum 4GB of RAM
+* Minimum 20GB of free disk space
+* If your system doesn't meet these requirements, the script will issue a warning and allow you to proceed at your own risk.
+
+## Usage
+Clone this repository to your local machine:
+```bash
+git clone https://github.com/samiul008ghub/soc_setup/
+```
+Navigate to the repository's directory:
+```bash
+cd soc_setup
+```
+Make the setup_script.sh executable:
+```bash
+chmod +x setup_script.sh
+```
+Execute the setup_script.sh:
+```bash
+ ./setup_script.sh
+```
+## Follow the on-screen prompts to choose which components you want to install and continue with the setup. Post-Installation Steps
+
+## After successfully running the script and completing the NIDS (Suricata) setup, consider the following post-installation steps:
